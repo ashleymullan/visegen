87375bfbff7398f96f9d9debb423d34d589601f2
Ashley Mullan <ashleymullan2000@gmail.com>
2024-11-05 09:50:08 -0600

    initial commit

---
+++ R/base_plot.R
@@ -0,0 +1,20 @@
+#' Generates a basic plot
+#'
+#' Provides the framework for a visualization
+#'
+#' @param x Independent Variable
+#' @param y Dependent Variable
+#' @return ggplot2 object
+#'
+#' @examples
+#' x <- rnorm(n = 100, mean = 0, sd = 1)
+#' y <- rnorm(n = 100, mean = 3, sd = 1)
+#'
+#; @export
+base_plot <- function(x,y){
+  df <- data.frame(x = x, y = y)
+  p <- df |>
+    ggplot2::ggplot(aes(x = x, y = y)) +
+    theme_bw()
+  p
+}
---
+++ R/visegen.R
@@ -0,0 +1,13 @@
+#' Creates Visualizations with Second Generation P-Values
+#'
+#' This section is under construction.
+#'
+#'
+#' @docType package
+#'
+#' @author Ashley E. Mullan \email{ashleymullan2000@gmail.com}
+#'
+#' @name visegen
+#' @import ggplot2
+#' @import dplyr
+NULL
